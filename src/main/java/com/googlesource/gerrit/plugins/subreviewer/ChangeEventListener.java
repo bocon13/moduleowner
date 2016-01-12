6efcd1076d84c38e129ba0218475a5c4c5b0ca21
Brian O'Connor <bocon@onlab.us>
2016-01-12 03:32:02 -0800

    Initial implementation of Module-Owner label

--- src/main/java/com/googlesource/gerrit/plugins/subreviewer/ChangeEventListener.java
+++ src/main/java/com/googlesource/gerrit/plugins/subreviewer/ChangeEventListener.java
@@ -1,16 +1,28 @@
 package com.googlesource.gerrit.plugins.subreviewer;
 
+import autovalue.shaded.com.google.common.common.collect.Lists;
 import com.google.gerrit.common.EventListener;
+import com.google.gerrit.common.TimeUtil;
+import com.google.gerrit.common.data.LabelType;
+import com.google.gerrit.common.data.LabelTypes;
 import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.reviewdb.client.PatchSet;
+import com.google.gerrit.reviewdb.client.PatchSetApproval;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.ApprovalsUtil;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.data.ApprovalAttribute;
 import com.google.gerrit.server.events.CommentAddedEvent;
 import com.google.gerrit.server.events.Event;
 import com.google.gerrit.server.events.PatchSetCreatedEvent;
 import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.git.WorkQueue;
+import com.google.gerrit.server.notedb.ChangeUpdate;
+import com.google.gerrit.server.project.ChangeControl;
+import com.google.gerrit.server.project.ProjectCache;
+import com.google.gerrit.server.project.ProjectState;
 import com.google.gerrit.server.util.RequestContext;
 import com.google.gerrit.server.util.ThreadLocalRequestContext;
 import com.google.gwtorm.server.OrmException;
@@ -27,6 +39,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Listener for change events, specifically patch set created events.
@@ -37,29 +51,44 @@ class ChangeEventListener implements EventListener {
 
     private final GitRepositoryManager repoManager;
     private final WorkQueue workQueue;
-    private final IdentifiedUser.GenericFactory identifiedUserFactory;
+
+    private final ChangeControl.GenericFactory changeControlFactory;
+    private final IdentifiedUser.GenericFactory userFactory;
+    private final ReviewersByOwnership.Factory reviewersFactory;
+    private final ChangeUpdate.Factory changeFactory;
+
+    private final ApprovalsUtil approvalsUtil;
     private final ThreadLocalRequestContext tl;
     private final SchemaFactory<ReviewDb> schemaFactory;
+
     private final ModuleOwnerConfigCache moduleOwnerConfigCache;
-    private final ReviewersByOwnership.Factory reviewersFactory;
+    private final ProjectCache projectCache;
     private ReviewDb db;
 
     @Inject
     ChangeEventListener(
             final GitRepositoryManager repoManager,
             final WorkQueue workQueue,
-            final IdentifiedUser.GenericFactory identifiedUserFactory,
+            final IdentifiedUser.GenericFactory userFactory,
+            final ApprovalsUtil approvalsUtil,
+            final ChangeControl.GenericFactory changeControlFactory,
             final ThreadLocalRequestContext tl,
             final SchemaFactory<ReviewDb> schemaFactory,
             final ModuleOwnerConfigCache moduleOwnerConfigCache,
-            final ReviewersByOwnership.Factory reviewersFactory) {
+            final ProjectCache projectCache,
+            final ReviewersByOwnership.Factory reviewersFactory,
+            final ChangeUpdate.Factory changeFactory) {
         this.repoManager = repoManager;
         this.workQueue = workQueue;
-        this.identifiedUserFactory = identifiedUserFactory;
+        this.userFactory = userFactory;
+        this.approvalsUtil = approvalsUtil;
+        this.changeControlFactory = changeControlFactory;
         this.tl = tl;
         this.schemaFactory = schemaFactory;
         this.moduleOwnerConfigCache = moduleOwnerConfigCache;
         this.reviewersFactory = reviewersFactory;
+        this.projectCache = projectCache;
+        this.changeFactory = changeFactory;
     }
 
     @Override
@@ -69,7 +98,7 @@ class ChangeEventListener implements EventListener {
             addReviewers((PatchSetCreatedEvent) event);
         } else if (event instanceof CommentAddedEvent) {
             // TODO New review available, add owner label if appropriate
-            return;
+            updateLabels((CommentAddedEvent) event);
         }
         // else, dropping event
     }
@@ -102,16 +131,124 @@ class ChangeEventListener implements EventListener {
                     rw.parseCommit(ObjectId.fromString(event.patchSet.revision));
 
             //TODO consider moving to RequestScopePropagator
-            final Runnable task =
-                    reviewersFactory.create(projectName, commit, change, repo);
-            workQueue.getDefaultQueue().submit(new Runnable() {
-                @Override
-                public void run() {
+            runTaskOnWorkQueue(reviewersFactory.create(projectName, commit, change, repo),
+                               change);
+
+        } catch (RepositoryNotFoundException e) {
+            log.warn("Repo not found: {}", projectName.get(), e);
+        } catch (IOException e) {
+            log.warn("IO Exception trying to get repo: {}", projectName.get(), e);
+        } catch (OrmException e) {
+            log.warn("OrmException while adding reviewers for: {}", projectName.get(), e);
+        }
+    }
+
+    private void updateLabels(CommentAddedEvent event) {
+        log.info("comment {} by {} with {}", event.comment, event.author.name, event.approvals);
+        if (event.approvals != null) {
+            for (ApprovalAttribute a : event.approvals) {
+                log.info("approval of type {} with desc {}/ value {}",
+                         a.type, a.description, a.value);
+            }
+        }
+
+        String user = null;
+        if (event.author != null) {
+            if (event.author.username != null) {
+                user = event.author.username;
+            } else if (event.author.email != null) {
+                user = event.author.email;
+            } else if (event.author.name != null) {
+                user = event.author.name;
+            }
+        }
+
+        // ^^^^^^ user filter
+
+        ModuleOwnerConfig moduleOwnerConfig = moduleOwnerConfigCache.get(event.getProjectNameKey());
+
+        try (Repository repo = repoManager.openRepository(event.getProjectNameKey());
+             ReviewDb reviewDb = schemaFactory.open()) {
+            Change.Id changeId = new Change.Id(Integer.parseInt(event.change.number));
+            final Change change = reviewDb.changes().get(changeId);
+            if (change == null) {
+                log.warn("Change {} not found.", changeId.get());
+                return;
+            }
+
+            PatchSet.Id psId = new PatchSet.Id(changeId, Integer.parseInt(event.patchSet.number));
+            PatchSet patchSet = reviewDb.patchSets().get(psId);
+            if (patchSet == null) {
+                log.warn("Patch set " + psId.get() + " not found.");
+                return;
+            }
+
+            final RevWalk rw = new RevWalk(repo);
+            final RevCommit commit =
+                    rw.parseCommit(ObjectId.fromString(event.patchSet.revision));
+
+
+            ProjectState projectState = projectCache.get(change.getProject());
+            LabelTypes labelTypes = projectState.getLabelTypes();
+            log.info("labels: {}", labelTypes);
+            LabelType codeReviewLabel = labelTypes.byLabel("Code-Review");
+            LabelType moduleOwnerLabel = labelTypes.byLabel("Module-Owner");
+
+            List<PatchSetApproval> existingApprovals = reviewDb.patchSetApprovals()
+                    .byChange(changeId).toList();
+            List<PatchSetApproval> moduleOwnerApprovals = Lists.newArrayList();
+            for (PatchSetApproval approval : existingApprovals) {
+                if (approval.isSubmit()) {
+                    continue;
+                }
+
+                // FIXME need to clear if CR == 0
+                // FIXME run in the background???
+
+                if (Objects.equals(codeReviewLabel.getName(), approval.getLabel()) &&
+                    moduleOwnerConfig.isModuleOwner(approval.getAccountId(), repo, commit)) {
+
+                    log.info("found approval:" + approval);
+                    PatchSetApproval psa = new PatchSetApproval(
+                            new PatchSetApproval.Key(
+                                    approval.getPatchSetId(),
+                                    approval.getAccountId(),
+                                    labelTypes.byLabel("Module-Owner").getLabelId()),
+                            approval.getValue(),
+                            TimeUtil.nowTs());
+                    moduleOwnerApprovals.add(psa);
+                }
+            }
+
+            reviewDb.changes().beginTransaction(change.getId());
+            try {
+                // FIXME conditional update / delete
+                reviewDb.patchSetApprovals().upsert(moduleOwnerApprovals);
+                reviewDb.commit();
+                log.info("success {}", moduleOwnerApprovals);
+            } catch (Exception e) {
+                log.warn("db write exception", e);
+            } finally {
+                reviewDb.rollback();
+            }
+        } catch (OrmException e) {
+            log.warn("OrmException while updating labels", e);
+        } catch (RepositoryNotFoundException e) {
+            e.printStackTrace(); //FIXME
+        } catch (IOException e) {
+            e.printStackTrace(); //FIXME
+        }
+    }
+
+    private void runTaskOnWorkQueue(final Runnable task, final Change change) {
+        workQueue.getDefaultQueue().submit(new Runnable() {
+            @Override
+            public void run() {
                 RequestContext old = tl.setContext(new RequestContext() {
 
                     @Override
                     public CurrentUser getCurrentUser() {
-                        return identifiedUserFactory.create(change.getOwner());
+                        return userFactory.create(change.getOwner());
                     }
 
                     @Override
@@ -140,15 +277,7 @@ class ChangeEventListener implements EventListener {
                         db = null;
                     }
                 }
-                }
-            });
-
-        } catch (RepositoryNotFoundException e) {
-            log.warn("Repo not found: {}", projectName.get(), e);
-        } catch (IOException e) {
-            log.warn("IO Exception trying to get repo: {}", projectName.get(), e);
-        } catch (OrmException e) {
-            log.warn("OrmException for repo: {}", projectName.get(), e);
-        }
+            }
+        });
     }
 }
