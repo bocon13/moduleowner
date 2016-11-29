0ff61936fe2d41c2e7468b212621c0d25e624ba8
Brian O'Connor <bocon@onlab.us>
2016-11-28 16:26:38 -0800

    Simplifying MergerUserValidator

--- src/main/java/com/googlesource/gerrit/plugins/moduleowner/MergeUserValidator.java
+++ src/main/java/com/googlesource/gerrit/plugins/moduleowner/MergeUserValidator.java
@@ -2,12 +2,9 @@ package com.googlesource.gerrit.plugins.moduleowner;
 
 import com.google.gerrit.reviewdb.client.Branch;
 import com.google.gerrit.reviewdb.client.PatchSet;
-import com.google.gerrit.reviewdb.client.PatchSetApproval;
 import com.google.gerrit.reviewdb.server.ReviewDb;
-import com.google.gerrit.server.ApprovalsUtil;
 import com.google.gerrit.server.IdentifiedUser;
 import com.google.gerrit.server.git.CodeReviewCommit;
-import com.google.gerrit.server.git.strategy.CommitMergeStatus;
 import com.google.gerrit.server.git.validators.MergeValidationException;
 import com.google.gerrit.server.git.validators.MergeValidationListener;
 import com.google.gerrit.server.project.ProjectState;
@@ -32,23 +29,13 @@ public class MergeUserValidator implements MergeValidationListener {
     private static final Logger log =
             LoggerFactory.getLogger(MergeUserValidator.class);
 
-    private final IdentifiedUser.GenericFactory identifiedUserFactory;
     private final Provider<ReviewDb> reviewDb;
-    private final ApprovalsUtil approvalsUtil;
     private final ModuleOwnerConfigCache configFactory;
 
-    // Because there is 'No user on merge thread' we need to get the
-    // identified user from IdentifiedUser.GenericFactory, this is
-    // normally not needed and you can, in many cases, just use
-    // Provider<CurrentUser>, unfortunately not this one.
     @Inject
-    MergeUserValidator(IdentifiedUser.GenericFactory identifiedUserFactory,
-                       Provider<ReviewDb> reviewDb,
-                       ApprovalsUtil approvalsUtil,
+    MergeUserValidator(Provider<ReviewDb> reviewDb,
                        ModuleOwnerConfigCache configFactory) {
-        this.identifiedUserFactory = identifiedUserFactory;
         this.reviewDb = reviewDb;
-        this.approvalsUtil = approvalsUtil;
         this.configFactory = configFactory;
     }
 
@@ -60,23 +47,14 @@ public class MergeUserValidator implements MergeValidationListener {
                            ProjectState destProject, Branch.NameKey destBranch,
                            PatchSet.Id patchSetId, IdentifiedUser caller)
             throws MergeValidationException {
-        PatchSetApproval psa =
-                approvalsUtil.getSubmitter(reviewDb.get(), commit.notes(), patchSetId);
-        if (psa == null) {
-            throw new MergeValidationException("no patchset found");
-        }
-
-        IdentifiedUser submitter =
-                identifiedUserFactory.create(psa.getAccountId());
-
         ModuleOwnerConfig config = configFactory.get(destProject.getProject().getNameKey());
         if (config != null && config.isEnabled() &&
-                !config.isModuleOwner(submitter.getAccountId(), repo, commit)) {
+                !config.isModuleOwner(caller.getAccountId(), repo, commit)) {
             throw new MergeValidationException(DENY_STATUS);
         }
 
         log.info("user {} submitted commit {}/{}",
-                 submitter.getUserName(),
+                 caller.getUserName(),
                  destProject.getProject().getNameKey(),
                  commit.getId().getName());
     }
